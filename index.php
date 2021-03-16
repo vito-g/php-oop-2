60cdfd41593d993fa549a63f81d905f61c1d3da3
Vito Gentile <gentilevito1076@gmail.com>
2021-03-16 15:55:30 +0100

    Def Classi Figlie Di Product

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
 
-
+//Parent Class
 class Product {
 
 
 }
+//End Parent Class
+
+//Children Classes
+class Food extends Product {
+
+}
+
+class Hygiene extends Product {
+
+}
+
+class Stationery extends Product {
+
+}
+//End Children Classes
+
 
 
 class User {
